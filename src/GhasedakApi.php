3a4563df26a63df19c2a5b1dfca3b9b91b570c33
ahoseinpour <31063471+ahoseinpour@users.noreply.github.com>
2019-06-17 09:37:25 +0430

    Update GhasedakApi.php
    chnage argument

--- src/GhasedakApi.php
+++ src/GhasedakApi.php
@@ -75,7 +75,7 @@ class GhasedakApi
         }
     }
 
-    public function SendSimple($linenumber = null, $receptor, $message, $senddate = null, $checkid = null)
+    public function SendSimple($receptor, $message , $linenumber = null, $senddate = null, $checkid = null)
     {
         $path = 'sms/send/simple';
         $params = array(
